e32f54d4d183d9a9dcf275a81027bec63d948500
Brad Dougherty <me@brad.is>
2019-09-26 23:52:38 -0400

    Update dev dependencies

--- test.js
+++ test.js
@@ -2,24 +2,26 @@ import test from 'ava';
 import execa from 'execa';
 
 test('help output', async (t) => {
-	const result = await execa.stdout('./cli.js', ['--help']);
-	t.true(result.length > 0);
+	const { stdout } = await execa('./cli.js', ['--help']);
+	t.true(stdout.length > 0);
 });
 
 test('stdin', async (t) => {
-	const result = await execa.stdout('./cli.js', [], {
+	const { stdout } = await execa('./cli.js', [], {
 		input: 'friday deploy, good luck!'
 	});
 
-	t.true(result.includes('friday'));
-	t.true(result.includes('deploy,'));
-	t.true(result.includes('good luck!'));
+	t.true(stdout.includes('friday'));
+	t.true(stdout.includes('deploy,'));
+	t.true(stdout.includes('good luck!'));
 });
 
 test('input args', async (t) => {
-	const result = await execa.stdout('./cli.js', ['friday', 'deploy,', 'good', 'luck']);
+	const { stdout } = await execa('./cli.js', ['friday', 'deploy,', 'good', 'luck!'], {
+		input: ''
+	});
 
-	t.true(result.includes('friday'));
-	t.true(result.includes('deploy,'));
-	t.true(result.includes('good luck!'));
+	t.true(stdout.includes('friday'));
+	t.true(stdout.includes('deploy,'));
+	t.true(stdout.includes('good luck!'));
 });
